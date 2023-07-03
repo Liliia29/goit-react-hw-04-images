daafbdef115bf68978a51ea52b3cabfcc3b711f6
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-07-03 21:51:24 +0300

    react-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import { getImagesAPI } from '../api';
 import { normalizeHits } from '../normalizedHits';
 import { ImageGallery } from 'components/ImageGallery';
@@ -12,96 +12,83 @@ import { AppWrapper, Error } from './App.styled';
 const imagesPerPage = 12;
 
 export const App = () => {
-    const [images, setImages] = useState([]);
-    const [query, setQuery] = useState('');
-    const [currentPage, setCurrentPage] = useState(1);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading]  = useState(false),
-    const [isLastPage, setIsLastPage] = useState(false);
-    const abortCtrl = useState();
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const abortCtrl = useRef();
 
-    useEffect(() => {
-      if ( query === '') {
-        return;
-      }
-
-      const getImages = async () => {
-         
-        if (abortCtrl.current) {
-          abortCtrl.current.abort();
-        }
-    
-        abortCtrl.current = new AbortController();
-
-        try {
-          setIsLoading({ isLoading: true });
-    
-          const data = await getImagesAPI(
-            query,
-            currentPage,
-            this.abortCtrl.signal
-          );
-    
-          const normalizedHits = normalizeHits(data.hits);
-    
-          this.setState(prevState => ({
-            images: [...prevState.images, ...normalizedHits],
-            isLastPage:
-              prevState.images.length + normalizedHits.length >= data.totalHits,
-            error: null,
-          }));
-        } catch (error) {
-          if (error.code !== 'ERR_CANCELED') {
-            this.setState({ error: error.message });
-          }
-        } finally {
-          this.setState({ isLoading: false });
-        }
-      };
-    })
-
-    
-
-    
-
-  handleSearchSubmit = query => {
-    if (this.state.query === query) {
+  useEffect(() => {
+    if (query === '') {
       return;
     }
 
-    this.setState({
-      query,
-      images: [],
-      currentPage: 1,
-      error: null,
-      isLastPage: false,
-    });
+    const getImages = async () => {
+      if (abortCtrl.current) {
+        abortCtrl.current.abort();
+      }
+
+      abortCtrl.current = new AbortController();
+
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const data = await getImagesAPI(
+          query,
+          currentPage,
+          imagesPerPage,
+          abortCtrl.current.signal
+        );
+
+        const normalizedHits = normalizeHits(data.hits);
+
+        setImages(state => [...state, ...normalizedHits]);
+        setIsLastPage(currentPage >= Math.ceil(data.totalHits / imagesPerPage));
+        setError(null);
+      } catch (error) {
+        if (error.code !== 'ERR_CANCELED') {
+          setError(error.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getImages();
+  }, [currentPage, query]);
+
+  const handleSearchSubmit = newQuery => {
+    if (newQuery === query) {
+      return;
+    }
+
+    setQuery(newQuery);
+    setCurrentPage(1);
+    setImages([]);
+    setError(null);
+    setIsLastPage(false);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
-    }));
+  const loadMore = () => {
+    setCurrentPage(state => state + 1);
   };
 
-  render() {
-    const { images, isLoading, error, isLastPage } = this.state;
+  return (
+    <AppWrapper>
+      <ToastContainer autoClose={2500} />
+      <Searchbar onSubmit={handleSearchSubmit} />
 
-    return (
-      <AppWrapper>
-        <ToastContainer autoClose={2500} />
-        <Searchbar onSubmit={this.handleSearchSubmit} />
+      {error && <Error>Error: {error} </Error>}
 
-        {error && <Error>Error: {error} </Error>}
+      <ImageGallery images={images} />
 
-        <ImageGallery images={images} />
+      {isLoading && <Loader />}
 
-        {isLoading && <Loader />}
-
-        {!isLoading && images.length > 0 && !isLastPage && (
-          <Button onClick={this.loadMore} />
-        )}
-      </AppWrapper>
-    );
-  }
-}
+      {!isLoading && images.length > 0 && !isLastPage && (
+        <Button onClick={loadMore} />
+      )}
+    </AppWrapper>
+  );
+};
